9517c32f076cc422856dc5ac39e0b9655b69bad4
CodeChoreographer <cedric.baumann94@gmail.com>
2024-09-26 14:31:40 +0200

    Version 1.2 Added JavaDoc und SpringDoc Bugfixes: - Bugs: CategoryID is always Null

--- src/main/java/com/modul295_lb1/productmanager/auth/SecurityConfig.java
+++ src/main/java/com/modul295_lb1/productmanager/auth/SecurityConfig.java
@@ -8,44 +8,57 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/**
+ * Konfiguriert die Sicherheitsrichtlinien für die Anwendung,
+ * einschliesslich der Authentifizierung und Autorisierung von Benutzern.
+ */
 @Configuration
 public class SecurityConfig {
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
 
+    /**
+     * Erstellt die Sicherheitsfilter für die Anwendung.
+     *
+     * @param http das HttpSecurity-Objekt zur Konfiguration
+     * @return die konfigurierte SecurityFilterChain
+     * @throws Exception Fehlermeldungen, wenn ein Fehler bei der Konfiguration auftritt
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // JWT-Filter vor dem Standard-Authentifizierungsfilter hinzufügen
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-        http.csrf().disable();  // CSRF-Schutz deaktivieren, da JWT verwendet wird
+
+        // CSRF-Schutz deaktivieren, da JWT verwendet wird
+        http.csrf().disable();
 
         http
                 .httpBasic().disable()  // HTTP Basic Authentication deaktivieren
                 .authorizeHttpRequests(authz -> authz
-                        // Benutzerregistrierung und -login
+                        // Erlaubt allen Benutzern die Registrierung und den Login
                         .requestMatchers("/users/register").permitAll()
                         .requestMatchers("/users/login").permitAll()
 
-                        // Benutzerdaten (Eigene Daten anzeigen und Admin-Berechtigungen)
+                        // Erfordert Authentifizierung für Benutzerdaten
                         .requestMatchers("/users/me").authenticated()
+                        // Erfordert Admin-Rolle für Zugriff auf andere Benutzerdaten
                         .requestMatchers("/users/{id}").hasAuthority("ROLE_ADMIN")
 
-                        // Produkte (Erstellen, Bearbeiten und Löschen nur für Admins)
-                        .requestMatchers(HttpMethod.POST,"/products").hasAuthority("ROLE_ADMIN")  // POST: Produkt hinzufügen nur für Admin
-                        .requestMatchers("/products/{id}").permitAll()  // GET: Produktdetails für alle sichtbar
-                        .requestMatchers( HttpMethod.PUT ,"/products/{id}").hasAuthority("ROLE_ADMIN")  // PUT: Nur Admin darf Produkte bearbeiten
-                        .requestMatchers(HttpMethod.DELETE,"/products/{id}").hasAuthority("ROLE_ADMIN")  // DELETE: Nur Admin darf Produkte löschen
-                        .requestMatchers("/products/**").permitAll()  // GET: Produktliste für alle
+                        // Produkte und Kategorien: Erlaubte Operationen basierend auf den Berechtigungen
+                        .requestMatchers(HttpMethod.POST, "/products").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/products/{id}").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/products/{id}").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/products/{id}").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/products/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/categories").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/categories/{id}").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/categories/{id}").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/categories/{id}").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/categories/{id}/products").permitAll()
+                        .requestMatchers("/categories/**").permitAll()
 
-                        // Produktkategorien (Erstellen, Bearbeiten und Löschen nur für Admins)
-                        .requestMatchers( HttpMethod.POST,"/categories").hasAuthority("ROLE_ADMIN")  // POST: Nur Admins dürfen Kategorien hinzufügen
-                        .requestMatchers("/categories/{id}").permitAll()  // GET: Kategoriedetails für alle sichtbar
-                        .requestMatchers(HttpMethod.PUT, "/categories/{id}").hasAuthority("ROLE_ADMIN") // PUT: Nur Admin darf Kategorien bearbeiten
-                        .requestMatchers(HttpMethod.DELETE, "/categories/{id}").hasAuthority("ROLE_ADMIN")  // DELETE: Nur Admin darf Kategorien löschen
-                        .requestMatchers("/categories/{id}/products").permitAll()  // GET: Alle Produkte in der Kategorie
-                        .requestMatchers("/categories/**").permitAll()  // GET: Alle Kategorien
-
-                        // API-Dokumentation öffentlich zugänglich machen
+                        // API-Dokumentation ist öffentlich zugänglich
                         .requestMatchers("/swagger-ui/index.html").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
@@ -54,6 +67,6 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
 
-        return http.build();  // SecurityFilterChain zurückgeben
+        return http.build();  // Gibt die konfigurierte SecurityFilterChain zurück
     }
 }
--- src/main/java/com/modul295_lb1/productmanager/auth/TokenService.java
+++ src/main/java/com/modul295_lb1/productmanager/auth/TokenService.java
@@ -4,28 +4,47 @@ import com.modul295_lb1.productmanager.resources.users.UserData;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
-//import static com.modul295_lb1.productmanager.resources.users.UserService.SECRET_KEY;
-
+/**
+ * Dienstklasse zur Verwaltung von JWT-Token.
+ * Diese Klasse bietet Funktionen zur Generierung von Token für Benutzer.
+ */
 @Service
 public class TokenService {
 
+    // Geheimschlüssel für die Token-Signierung
     private final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
+    /**
+     * Generiert ein JWT-Token für den angegebenen Benutzer.
+     *
+     * @param userData die Benutzerdaten, die für die Token-Generierung verwendet werden
+     * @return das generierte JWT-Token als String
+     */
+    @Operation(summary = "Generiert ein JWT-Token für den Benutzer",
+            description = "Erstellt ein JWT-Token, das die E-Mail des Benutzers und seine Rollen enthält.")
     public String generateToken(UserData userData) {
         return Jwts.builder()
-                .setSubject(userData.getEmail())
-                .claim("roles", userData.getRoles())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(SECRET_KEY)
-                .compact();
+                .setSubject(userData.getEmail())  // Setzt die Benutzer-E-Mail als Subjekt des Tokens
+                .claim("roles", userData.getRoles())  // Fügt die Rollen des Benutzers hinzu
+                .setIssuedAt(new Date(System.currentTimeMillis()))  // Setzt das Ausstellungsdatum
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))  // Setzt das Ablaufdatum auf 10 Stunden
+                .signWith(SECRET_KEY)  // Signiert das Token mit dem Geheimschlüssel
+                .compact();  // Kompaktierung des Tokens
     }
 
+    /**
+     * Gibt den geheimen Schlüssel zurück, der für die Token-Signierung verwendet wird.
+     *
+     * @return der geheime Schlüssel
+     */
+    @Operation(summary = "Gibt den geheimen Schlüssel zurück",
+            description = "Ruft den geheimen Schlüssel ab, der für die Signierung von JWT-Token verwendet wird.")
     public SecretKey getSecretKey() {
         return SECRET_KEY;
     }
